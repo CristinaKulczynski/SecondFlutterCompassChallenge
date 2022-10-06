ba9afeefb767f01ab7d65dafc9c9a00ba1615827
Cristina Kulczynski <cristinakulczynski@gmail.com>
2022-10-06 00:34:01 -0300

    Atualização da main, criação de pastas e da primeira tela a splash_screen.

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:desafio_sprint_dois/screens/splash_screen.dart';
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: SplashScreen()
+    );
+  }
+}
+
+
