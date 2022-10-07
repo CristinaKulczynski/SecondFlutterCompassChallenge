8e1c0bb29e9318c1858313becd97a070a6665c43
Cristina Kulczynski <cristinakulczynski@gmail.com>
2022-10-07 16:32:17 -0300

    Adicionando imagens na segunda tela.

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -7,10 +7,33 @@ class WelcomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
           Container(
-            height: 469,
+            height: 469, // Altura
+            width: 395, // Largura
             color: const Color.fromRGBO(255, 164, 81, 1),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Image.asset('assets/images/fruit_welcome2.png'),
+                    Container(
+                      width: 55,
+                    ),
+                  ],
+                ),
+                Image.asset('assets/images/fruit_welcome.png'),
+                Padding(
+                  padding: const EdgeInsets.all(7.5),
+                  child: Image.asset('assets/images/ellipse_welcome.png'),
+                ),
+                Container(height: 25.5 // Altura
+                ),
+              ],
+            ),
           ),
         ],
       ),
