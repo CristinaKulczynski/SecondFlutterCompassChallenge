9488d7cf12ec8368688c6c7a08bd2e0ebc8f8509
Cristina Kulczynski <cristinakulczynski@gmail.com>
2022-10-08 01:14:52 -0300

    Botão acrescentado!

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 
-class WelcomeScreen extends StatelessWidget {
+class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({Key? key}) : super(key: key);
 
+  @override
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -38,29 +43,56 @@ class WelcomeScreen extends StatelessWidget {
             height: 41,
           ),
           SizedBox(
-            width: 350, // Largura
+            width: 330, // Largura
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
-              children:const[
-                Text(
+              children: [
+                const Text(
                   'Get The Freshest Fruit Salad Combo',
                   style: TextStyle(
                     fontSize: 15,
                     fontFamily: 'Poppins',
                     fontWeight: FontWeight.bold,
                     color: Color.fromRGBO(39, 33, 77, 1),
-                        ),
+                  ),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 5,
                 ),
-                Text(
+                const Text(
                   'We deliver the best and freshest fruit salad \nin town. Order for a combo today!!!',
                   style: TextStyle(
-                    fontSize: 15,
+                    fontSize: 14,
                     height: 1.6,
                     fontFamily: 'Poppins',
                     color: Color.fromRGBO(93, 87, 126, 1),
+                  ),
+                ),
+                const SizedBox(
+                  height: 70,
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(
+                      const Color.fromRGBO(255, 164, 81, 1),
+                    ),
+                    fixedSize: MaterialStateProperty.all(
+                      const Size(327, 56),
+                    ),
+                    shape: MaterialStateProperty.all(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                    ),
+                  ),
+                  child: const Text(
+                    'Let’s Continue',
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontFamily: 'Poppins',
+                      color: Colors.white,
+                    ),
                   ),
                 ),
               ],
